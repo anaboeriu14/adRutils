1b4ce792a807cbfd442e0229fd68a09020af821e
anaboeriu14 <ilincaboeriu@gmail.com>
2026-04-28 17:51:50 -0700

    chore: move suggested packages out of imports.R

--- R/imports.R
+++ R/imports.R
@@ -1,28 +1,24 @@
 #' @title adRutils: A collection of R utility functions
-#' @description This package provides various utility functions for data manipulation, imputation, and other common tasks.
+#' @description This package provides various utility functions for data
+#'   manipulation, modeling, and meta-analysis support.
 #' @name adRutils
 #'
 #' @import dplyr
 #' @import cli
 #' @import purrr
 #' @import stringr
-#' @import lme4
+#' @importFrom dplyr filter lag
 #' @importFrom magrittr %>%
 #' @importFrom stats setNames complete.cases pnorm lm nobs as.formula
-#' pairwise.t.test qnorm chisq.test p.adjust fisher.test
+#'   pairwise.t.test qnorm chisq.test p.adjust fisher.test
 #' @importFrom tibble tibble as_tibble
-#' @importFrom rlang .data sym  %||% .data := enquo as_name enquos eval_tidy
+#' @importFrom rlang .data sym `%||%` `:=` enquo as_name enquos eval_tidy
 #' @importFrom readr read_csv
 #' @importFrom janitor clean_names
-#' @importFrom missForest missForest
-#' @importFrom doParallel registerDoParallel
 #' @importFrom here here
 #' @importFrom utils head object.size read.csv combn
 #' @importFrom glue glue
 #' @importFrom broom tidy
 #' @importFrom tidyr expand_grid unite replace_na unnest
-#' @importFrom parameters standardize_parameters
-#' @importFrom metafor rma
-
 
 NULL
